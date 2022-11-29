6d734d7c427c7cbbfe2cb220be3a22db3df4c6a6
valmirt <valmir.torres@netpos.com.br>
2022-11-29 19:06:56 -0300

    Initial Commit

---
+++ TestMV/TestMVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMVApp.swift
+//  TestMV
+//
+//  Created by Valmir Torres on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestMVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
